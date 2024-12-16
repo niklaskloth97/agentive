762689d7256885131336130d5363d8000743e4b2
Niklas Kloth <niklas.kloth@gmail.com>
2024-12-16 11:45:34 +0100

    started about page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -4,14 +4,33 @@ export default function About() {
   return (
       <div className="container mx-auto py-8">
         <h1 className="text-3xl font-bold mb-6">About AGENTIVE</h1>
+        <h2 className="text-xl font-semibold mb-4"> Stimulating Multilingual Learning in Early Childhood Education </h2>
         <div className="flex flex-col md:flex-row gap-8">
-          <div className="md:w-1/2">
+          {/* <div className="md:w-1/2">
             <Image src="/placeholder.svg" alt="About AGENTIVE" width={500} height={300} className="rounded-lg mb-4" />
-          </div>
-          <div className="md:w-1/2">
+          </div> */}
+          <div /*className="md:w-1/2"*/>
+            <h2 className="text-m font-semibold mb-4">Our Vision: Promoting Multilingualism in Early Childhood</h2>
             <p className="text-gray-600 mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            In the world – not just in Europe - where multiple languages coexist and shape today's societies, AGENTIVE aims to equip children with the tools to navigate and thrive in multilingual environments. Current research highlights the cognitive, social, and academic benefits of multilingualism, particularly when introduced early in life. AGENTIVE seeks to bridge the gap in early childhood language education by providing accessible, evidence-based resources to support multilingual literacy development. 
             </p>
+            <h2 className="text-m font-semibold mb-4">An EU-Funded Erasmus+ Initiative </h2>
+            <p className="text-gray-600 mb-4">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h2 className="text-m font-semibold mb-4">An EU-Funded Erasmus+ Initiative </h2>
+            <p className="text-gray-600 mb-4">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h2 className="text-m font-semibold mb-4">An EU-Funded Erasmus+ Initiative </h2>
+            <p className="text-gray-600 mb-4">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h2 className="text-m font-semibold mb-4">An EU-Funded Erasmus+ Initiative </h2>
+            <p className="text-gray-600 mb-4">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h2 className="text-m font-semibold mb-4">An EU-Funded Erasmus+ Initiative </h2>
             <p className="text-gray-600 mb-4">
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
---
+++ frontend/app/auth/route.ts
@@ -0,0 +1,3 @@
+import { handlers } from "@/auth" //reffering to root folder's auth.ts, where it is configured
+export const { GET, POST } = handlers
+// delete? for 
