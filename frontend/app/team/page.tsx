554d4a990bc74a8714d9719ae38b34ec6c54d07c
Niklas Kloth <niklas.kloth@gmail.com>
2024-12-17 20:31:28 +0100

    changed the overlay

--- frontend/app/team/page.tsx
+++ frontend/app/team/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 
 export default function Team() {
-
-
   const teamMembers = [
     { name: 'Claudine Kirsch', role: 'Project Manager', image: '/images/people/Claudine_Kirsch.webp' },
     { name: 'Jan vom Brocke', role: 'Platform Innovator', image: '/images/people/jan-vom-brocke.jpg' },
@@ -30,10 +28,11 @@ export default function Team() {
   return (
       <div className="container py-8 max-w-7xl mx-auto">
         <h1 className="text-3xl font-bold mb-6">The Team</h1>
+        <h2 className="text-xl text-gray-600 mb-8">The AGENTIVE project is made possible by:</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {teamMembers.map((member, index) => (
             <div key={index} className="text-center">
-              <Image src={member.image} alt={member.name} width={200} height={200} className="rounded-full mx-auto mb-4" />
+              <Image src={member.image} alt={member.name} width={200} height={200} className="rounded-full border-4 mx-auto mb-4" />
               <h2 className="text-xl font-semibold">{member.name}</h2>
               <p className="text-gray-600">{member.role}</p>
             </div>
@@ -43,7 +42,9 @@ export default function Team() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {partnerUniversities.map((member, index) => (
             <div key={index} className="text-center">
-              <Image src={member.image} alt={member.name} width={200} height={200} className="mx-auto mb-4" />
+              <div className="h-48 flex items-center justify-center">
+                <Image src={member.image} alt={member.name} width={200} height={200} className="mx-auto mb-4 object-contain" />
+              </div>
               <h2 className="text-xl font-semibold">{member.name}</h2>
               <p className="text-gray-600">{member.role}</p>
             </div>
