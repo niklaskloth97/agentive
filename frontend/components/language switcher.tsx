835d21e3d4082cfb993b6a02212c58df0cabe4e2
linhpth11 <halinh091110@gmail.com>
2025-01-30 23:39:18 +0100

    language component

---
+++ frontend/components/language switcher.tsx
@@ -0,0 +1,27 @@
+"use client";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"; 
+import { useTranslation } from "react-i18next";
+
+import * as React from "react";
+
+export default function LanguageDropdown() {
+    const { i18n } = useTranslation();
+    const onLanguageChange = (lang: string) => {
+        i18n.changeLanguage(lang);     
+    }; //change language function
+        return (
+      <DropdownMenu>
+        <DropdownMenuTrigger className="p-2 border rounded-md">🌐 Language</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onSelect={() => onLanguageChange("en")}>English</DropdownMenuItem>
+          <DropdownMenuItem onSelect={() => onLanguageChange("de")}>Deutsch</DropdownMenuItem>  //kann man auch mehr Sprachen hinzufügen
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+        
