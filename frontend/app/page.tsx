2bbd8b5a96d9f6a062a85befb0c9d7ad67bc7343
Alice Fiolka <afiolka@uni-muenster.de>
2025-02-01 17:26:13 +0100

    Änderungen Farbschema

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,32 @@
 export default function Home() {
   return (
       <div className="container mx-auto py-8">
-        <h1 className="text-3xl mb-6 text-green-800 font-bold dark:text-green-600">Welcome to AGENTIVE</h1>
-        <h1 className="text-xl mb-2 text-green-800 font-semibold dark:text-green-600 ">Boosting linguistic diversity in early childhood education through synergies
+        <h1 className="text-3xl mb-6  font-bold text-sky-600 dark:text-sky-400">Welcome to AGENTIVE</h1>
+        <h1 className="text-xl mb-2 font-semibold text-sky-600 dark:text-sky-400 ">Boosting linguistic diversity in early childhood education through synergies
         </h1>
         <p className="text-gray-600 mb-4 dark:text-white">
         Welcome to the AGENTIVE platform! As part of an EU-funded Erasmus+ initiative, we are creating innovative tools to promote language learning in Early Childhood Education (ECE). We empower children and foster professional development of educators. Our mission is to  provide accessible, open-source materials for language and literacy development, offer professional development training and stimulate school-university-business synergies.
         </p> <div className="flex flex-col md:flex-row gap-8"></div>
-        <h2 className="text-m mb-2 text-green-800 font-semibold dark:text-green-600" >What Can You Expect?</h2>
+        <h2 className="text-m mb-2 font-semibold text-sky-600 dark:text-sky-400" >What Can You Expect?</h2>
           <p className="text-gray-600 mb-2 dark:text-white"> 
           This website is just the beginning - think of it as a sneak peek into what is to come. AGENTIVE aims to create resources that celebrate linguistic diversity, promote inclusion, and enable children to thrive in a multilingual world.
             </p>
           <p className="text-gray-600 mb-4 dark:text-white"> 
           Our materials will be openly available under open-source licenses, such as Creative Commons, to ensure educators, parents, and schools can adapt and share them freely. Stay tuned as we develop and release these exciting resources in the coming months!
             </p>
-        <h2 className="text-m mb-2 text-green-800 font-semibold dark:text-green-600">Who Are We?</h2>
+        <h2 className="text-m mb-2 font-semibold text-sky-600 dark:text-sky-400">Who Are We?</h2>
           <p className="text-gray-600 mb-4 dark:text-white"> 
           AGENTIVE is a collaborative effort involving universities and organizations from Luxembourg, Switzerland, Greece, Slovenia, Germany and Italy. Together, we’re leveraging expertise across academia, schools, and businesses to drive innovation in multilingual education. Want to know more about our vision and objectives? Visit our About Us page.
             </p>
-        <h2 className="text-m text-green-800 mb-2 font-semibold dark:text-green-600">Curious About the Team Behind AGENTIVE?</h2>
+        <h2 className="text-m mb-2 font-semibold text-sky-600 dark:text-sky-400">Curious About the Team Behind AGENTIVE?</h2>
           <p className="text-gray-600 mb-4 dark:text-white"> 
           Our team is made up of passionate educators, researchers, and developers working together to make multilingual education accessible for everyone. Interested in meeting the people behind the project? Check out our team page.
             </p>
-        <h2 className="text-m mb-2 text-green-800 font-semibold dark:text-green-600">Follow Our Journey</h2>
+        <h2 className="text-m mb-2 font-semibold text-sky-600 dark:text-sky-400">Follow Our Journey</h2>
           <p className="text-gray-600 mb-4 dark:text-white"> 
           From project milestones to insights about multilingualism, we’ll be sharing updates, reflections, and behind-the-scenes moments on our blog. Want to stay in the loop? Read the latest updates here.
           </p> 
-        <h2 className="text-m text-green-800 mb-2 font-semibold dark:text-green-600">Stay Tuned!
+        <h2 className="text-m mb-2 font-semibold text-sky-600 dark:text-sky-400">Stay Tuned!
         </h2>
           <p className="text-gray-600 mb-2 dark:text-white">
           AGENTIVE has started on 1st October. In the coming months, we will design  8 pedagogical sets comprising stories in multiple languages and materials, which we will unveil in the Summer. Whether you’re a teacher, a parent, or simply curious about the world of multilingual education, there’s something here for you. 
