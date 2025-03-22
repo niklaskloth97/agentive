6526660853c258f80b0070dc37fff5e494f0421d
linhpth11 <halinh091110@gmail.com>
2025-03-22 18:19:15 +0700

    Added test page under /testing_linh

---
+++ frontend/app/testing_linh/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-primary">Test Page in About</h1>
+      </div>
+    );
+  }
+  
