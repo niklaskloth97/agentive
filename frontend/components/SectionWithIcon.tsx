2a187370a4ee87c3b4af4a1e14d41a40a2e36a00
Niklas Kloth <niklas.kloth@gmail.com>
2025-04-01 14:57:13 +0200

    redesign of about page

--- frontend/components/SectionWithIcon.tsx
+++ frontend/components/SectionWithIcon.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { cn } from '@/lib/utils';
+import React, {ReactNode} from 'react';
 
 interface SectionWithIconProps {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
   title: string;
   description: string;
+  className?: string;
 }
 
-const SectionWithIcon: React.FC<SectionWithIconProps> = ({ icon, title, description }) => {
+const SectionWithIcon: React.FC<SectionWithIconProps> = ({ icon, title, description, className }) => {
   return (
-    <motion.div whileHover={{ scale: 1.02 }} className="text-center bg-blue-50 p-8 rounded-2xl shadow-md my-8">
+    <motion.div whileHover={{ scale: 1.02 }} className={cn("text-center bg-blue-50 p-8 rounded-2xl shadow-md my-8", className)}>
       <h2 className="text-3xl font-bold text-blue-700 mb-4 flex items-center justify-center gap-3">
         {icon} {title}
       </h2>
