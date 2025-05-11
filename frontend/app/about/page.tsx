a2a12dbb1e24f38ba6bd55d280190717264cb283
Niklas Kloth <niklas.kloth@gmail.com>
2025-05-11 14:07:52 +0200

    updated texts according to feedback

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -8,27 +8,23 @@ export default function About() {
           <Image src="/placeholder.svg" alt="About AGENTIVE" width={500} height={300} className="rounded-lg mb-4" />
         </div> */}
         <div /*className="md:w-1/2"*/>
-          <h2 className="text-m font-semibold mb-2">Our Vision: Promoting Multilingualism in Early Childhood</h2>
+          <h2 className="text-m font-semibold mb-2">Our Vision: Promoting Multilingualism in ECE</h2>
           <p className="text-gray-600 dark:text-white mb-4">
-          Multiple languages coexist and shape today&apos;s societies, AGENTIVE aims to equip children with the tools to navigate and thrive in multilingual environments. Current research highlights the cognitive, social, and academic benefits of multilingualism, particularly when introduced early in life. AGENTIVE seeks to bridge the gap in early childhood language education by providing accessible, evidence-based resources to support multilingual literacy development.
+          Languages play an important role in children’s everyday lives in Europe; many speak several languages at home while also encountering further languages at school. Due to the cognitive, social, and academic benefits of multilingualism, many European countries have introduced multilingual education in Early Childhood Education (ECE). Indeed, children can learn languages successfully early in life provided they have child-friendly and stimulating conditions. While programmes for language awareness and early English thrive, teachers appear to be unsure of how to foster language learning and to lack multilingual resources. 
           </p>
           <p className="text-gray-600 dark:text-white mb-4">
-          Languages and multilingualism play an important role in children&apos;s everyday lives as many children grow up speaking several languages at home and in their setting of Early Childhood Education (ECE). Language skills beyond the family language(s) count as key qualifications in Europe. The early childhood phase is particularly important for multilingual language learning as children appear to develop skills in additional language(s) effortlessly, provided they have child-friendly and stimulating conditions in ECE.
+          AGENTIVE seeks to bridge this gap by providing multilingual evidence-based resources that support multilingual literacy development and are easily adaptable to various cultural and linguistic contexts. Professionals and parents can download our  
+          eight pedagogical sets (i.e., stories and follow-up activities) on our digital platform. AGENTIVE also offers seminars for student teachers and professional development courses for teachers to develop the digital and pedagogical competences needed to use our materials effectively. Our school-university-business synergy will contribute to the wide dissemination of the findings. 
+          </p>
+          <h2 className="text-m font-semibold mb-2">An EU-Funded Erasmus+ Initiative Across Sectors</h2>
+          <p className="text-gray-600 dark:text-white mb-4">
+          The AGENTIVE project is a groundbreaking initiative funded by the Erasmus+ programme of the European Union. Uniting universities and organizations from six countries, this pan-European partnership endorses the EU’s commitment to linguistic diversity, inclusion and digital transformation in education.  
           </p>
           <p className="text-gray-600 dark:text-white mb-4">
-          AGENTIVE aims to promote early language learning through creating eight pedagogical sets as well as a digital platform where professionals and parents can download our educational resources. Furthermore, the team offers professional development training to teachers and student teachers to help them use the materials effectively and develop digital competences. Our school-university-business synergy will contribute to the wide dissemination of the findings.
+          In regions like Luxembourg, Switzerland, Tyrol and Primorska, multiple languages are spoken side by side and children may learn two societal languages from an early age through daily interactions or language learning in preschool. Additionally, migration patterns across Europe have increased the language diversity in societies and classrooms. Programmes for language awareness and early English thrive. AGENTIVE addresses these realities by developing inclusive and accessible, multilingual learning materials that are adaptable to different cultural and linguistic contexts. 
           </p>
-          <h2 className="text-m font-semibold mb-2">An EU-Funded Erasmus+ Initiative</h2>
           <p className="text-gray-600 dark:text-white mb-4">
-          The AGENTIVE project is a groundbreaking initiative funded by the Erasmus+ programme of the European Union. It is a collaborative endeavour that unites universities and organizations from Luxembourg, Greece, Germany, Switzerland, Slovenia, and Italy to develop innovative multilingual educational resources for early childhood education (ECE) and to develop school-university and business synergies. This pan-European partnership emphasizes the EU’s commitment to fostering multilingualism and digital transformation in education.
-          </p>
-          <h2 className="text-m font-semibold mb-2">Collaboration Across Sectors</h2>
-          <p className="text-gray-600 dark:text-white mb-4">
-          Led by Prof. Dr. Claudine Kirsch at the University of Luxembourg, AGENTIVE brings together the University of Münster, the Free University of Bolzano, the University of Teacher Education of the Grisons (PHGR), the University of Primorska and Web2Learn. This consortium fosters synergies between academia and schools to create innovative multilingual education material. The collaboration is designed to ensure sustainable and impactful results that benefit teachers, students, parents and society as a whole.
-          </p>
-          <h2 className="text-m font-semibold mb-2">Addressing a Multilingual Europe</h2>
-          <p className="text-gray-600 dark:text-white mb-4">
-          In regions like Luxembourg, Switzerland, Tyrol and Primorska, multiple languages are spoken side by side and children may learn two societal languages from an early age through daily interactions or language learning in preschool. Additionally, migration patterns across Europe have increased the language diversity in societies and classrooms. Programmes for language awareness and early English thrive. AGENTIVE addresses these realities by developing inclusive and accessible, multilingual learning materials that are adaptable to different cultural and linguistic contexts.
+          Led by Prof. Dr. Claudine Kirsch, AGENTIVE brings together the University of Luxembourg, the University of Münster (Germany), the Free University of Bolzano (Italy), the University of Teacher Education of the Grisons (Switzerland), the University of Primorska (Slovenia) and Web2Learn (Greece). Our synergies between academia and schools enable us to to create innovative, evidence-based and sustainable multilingual education resources that benefit teachers, student teachers, parents, children and society as a whole. 
           </p>
           <h2 className="text-m font-semibold mb-2">Open Access to Resources</h2>
           <p className="text-gray-600 dark:text-white mb-4">
@@ -38,10 +34,10 @@ export default function About() {
           <p className='text-gray-600 dark:text-white mb-1'>AGENTIVE is guided by two key objectives:</p>
           <ol className="list-decimal ml-8 text-gray-600 dark:text-white mb-4">
             <li>
-            < span className="font-semibold">Enhance digital transformation of schools and universities through open access early language learning resources and state-of-the-art pedagogies and training: </span> By creating open-access, state-of-the-art pedagogical tools, the project supports the digital readiness of schools and universities. Training programs for pre- and in-service ECE teachers are integral to this goal.
+            Enhance digital transformation of schools and universities through open education resources and state-of-the-art pedagogies: By creating open-access, innovative pedagogical tools, the project supports the digital readiness of schools and universities. Training programs for pre- and in-service ECE teachers are integral to this goal. 
             </li>
             <li>
-            < span className="font-semibold">Leverage open, multilingual and cross-sectoral ECE through university-school-business synergies:  </span> AGENTIVE fosters partnerships between schools, universities, and businesses to innovate multilingual education. These collaborations ensure the materials and approaches developed are practical, effective, and forward-thinking.
+            Leverage open, multilingual and cross-sectoral ECE through university-school-business synergies: AGENTIVE fosters partnerships between schools, universities, and businesses to innovate multilingual education. These collaborations ensure that our pedagogical approaches and materials are appropriate, inclusive and forward-thinking. 
             </li>
           </ol>
           <h2 className="text-m font-semibold mb-2">Innovation and Impact</h2>
@@ -49,26 +45,23 @@ export default function About() {
             <p className='mb-1'>AGENTIVE’s approach is holistic and multidimensional, offering:</p>
             <ul className="list-disc ml-8">
             <li>
-            < span className="font-semibold">Digital Resources for Multilingual Literacy: </span> The project provides open, digital materials that promote the learning of languages and literacy, addressing a key gap in early childhood education.
+            Digital resources for multilingual literacy: The project provides open, digital materials that promote the learning of multiple languages and early literacy skills, addressing a key gap in early childhood education. 
             </li>
             <li>
-            < span className="font-semibold"> Equipping Educators: </span> By equipping pre- and in-service teachers with evidence-based strategies and digital competencies, AGENTIVE empowers educators to implement multilingual teaching effectively.
+           Equipping educators: By equipping pre- and in-service teachers with evidence-based strategies and digital competencies, AGENTIVE empowers professionals to implement multilingual teaching effectively. 
             </li>
             <li>
-            < span className="font-semibold"> Cross-Sector Collaboration:  </span> The synergy between academia and businesses drives the development of innovative educational tools and sustainable partnerships.
+            Cross-Sector collaboration: The synergy between academia and businesses drives the development of innovative educational tools and sustainable partnerships. 
             </li>
           </ul>
           </div>
-          <h2 className="text-m font-semibold mb-2">Alignment with EU Goals</h2>
-          <p className="text-gray-600 dark:text-white mb-4">
-              Aligned with the EU’s priorities for education, AGENTIVE promotes linguistic diversity, inclusion, and digital transformation. It embodies the EU’s vision of fostering a multilingual society. For this project, we are funded by Europe’s Erasmus+ Programme.
-          </p>
+         
           <h2 className="text-m font-semibold mb-2">A Model for the Future</h2>
           <p className="text-gray-600 dark:text-white mb-4">
-          By building on the expertise of its partners and leveraging the multilingual realities of Europe, AGENTIVE aims to set a benchmark for early childhood multilingual education. The project’s outcomes will contribute to shaping a more inclusive and innovative educational landscape across Europe.
+          By building on the expertise of its partners and leveraging the multilingual realities of Europe, AGENTIVE aims to set a benchmark for early childhood multilingual education. The project’s outcomes will contribute to more inclusive and innovative learning environments across Europe. 
           </p>
           <p className="text-gray-600 dark:text-white mb-4">
-          For more information about AGENTIVE, our resources, and upcoming events, please explore our platform. Together, we can inspire the next generation to embrace the power of multilingualism!
+          For more information about the AGENTIVE resources and upcoming events, please explore our platform. Together, we can inspire the next generation to embrace the power of multilingualism! 
           </p>
         </div>
       </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,34 +2,31 @@ export default function Home() {
   return (
       <div className="container mx-auto py-8 px-8">
         <h1 className="text-3xl mb-6  font-bold ">Welcome to AGENTIVE</h1>
-        <h1 className="text-xl mb-2 font-semibold ">Boosting linguistic diversity in early childhood education through synergies
+        <h1 className="text-xl mb-2 font-semibold ">Boosting linguistic diversity in early childhood education through synergies 
         </h1>
         <p className="text-gray-600 mb-4 dark:text-white">
-        Welcome to the AGENTIVE platform! As part of an EU-funded Erasmus+ initiative, we are creating innovative tools to promote language learning in Early Childhood Education (ECE). We empower children and foster professional development of educators. Our mission is to  provide accessible, open-source materials for language and literacy development, offer professional development training and stimulate school-university-business synergies.
+        Welcome to the AGENTIVE platform! As part of an EU-funded Erasmus+ initiative, we are creating innovative tools to promote language learning in Early Childhood Education (ECE). Our goal is to provide free evidence-based resources for language and literacy development in ECE, offer professional development materials for teachers and stimulate school-university-business synergies. 
         </p> <div className="flex flex-col md:flex-row gap-8"></div>
         <h2 className="text-m mb-2 font-semibold" >What Can You Expect?</h2>
           <p className="text-gray-600 mb-2 dark:text-white"> 
-          This website is just the beginning - think of it as a sneak peek into what is to come. AGENTIVE aims to create resources that celebrate linguistic diversity, promote inclusion, and enable children to thrive in a multilingual world.
-            </p>
-          <p className="text-gray-600 mb-4 dark:text-white"> 
-          Our materials will be openly available under open-source licenses, such as Creative Commons, to ensure educators, parents, and schools can adapt and share them freely. Stay tuned as we develop and release these exciting resources in the coming months!
+          AGENTIVE aims to create resources that celebrate linguistic diversity and enable teachers to create inclusive learning opportunities where children develop multilingual competence. Our materials will be openly available, enabling teachers and parents to adapt and share them freely. Stay tuned as we develop and release these exciting resources in the coming months! 
             </p>
         <h2 className="text-m mb-2 font-semibold ">Who Are We?</h2>
-          <p className="text-gray-600 mb-4 dark:text-white"> 
-          AGENTIVE is a collaborative effort involving universities and organizations from Luxembourg, Switzerland, Greece, Slovenia, Germany and Italy. Together, we’re leveraging expertise across academia, schools, and businesses to drive innovation in multilingual education. Want to know more about our vision and objectives? Visit our About Us page.
+            <p className="text-gray-600 mb-4 dark:text-white"> 
+            AGENTIVE involves universities and organizations from Luxembourg, Switzerland, Greece, Slovenia, Germany and Italy. Together, we're leveraging expertise across academia, schools, and businesses to drive innovation in multilingual education. Visit our <a href="/about" className="text-blue-600 hover:underline dark:text-blue-400">About Us</a> page.
             </p>
         <h2 className="text-m mb-2 font-semibold ">Curious About the Team Behind AGENTIVE?</h2>
           <p className="text-gray-600 mb-4 dark:text-white"> 
-          Our team is made up of passionate educators, researchers, and developers working together to make multilingual education accessible for everyone. Interested in meeting the people behind the project? Check out our team page.
+          Our team is made up of passionate researchers, teachers and developers working together to make multilingual education accessible for everyone. Check out our <a href="/team" className="text-blue-600 hover:underline dark:text-blue-400">team</a> page. 
             </p>
         <h2 className="text-m mb-2 font-semibold ">Follow Our Journey</h2>
           <p className="text-gray-600 mb-4 dark:text-white"> 
-          From project milestones to insights about multilingualism, we’ll be sharing updates, reflections, and behind-the-scenes moments on our blog. Want to stay in the loop? Read the latest updates here.
+          From project milestones to insights about multilingualism, we’ll be sharing updates, reflections, and behind-the-scenes moments on our blog. Read the latest updates <a href="/blog" className="text-blue-600 hover:underline dark:text-blue-400">here.</a> 
           </p> 
         <h2 className="text-m mb-2 font-semibold">Stay Tuned!
         </h2>
           <p className="text-gray-600 mb-2 dark:text-white">
-          AGENTIVE has started on 1st October. In the coming months, we will design  8 pedagogical sets comprising stories in multiple languages and materials, which we will unveil in the Summer. Whether you’re a teacher, a parent, or simply curious about the world of multilingual education, there’s something here for you. 
+          AGENTIVE has started on 1st October 2024. In the coming months, we will design 8 pedagogical sets comprising stories in multiple languages and materials, which we will unveil in the Summer. Whether you’re a teacher, a parent, or simply curious about the world of multilingual education, there’s something here for you.
             </p> 
             <p className="text-gray-600 mb-4 dark:text-white">
             Thanks for joining us on this journey—together, we’re shaping the future of early childhood multilingual education! 
