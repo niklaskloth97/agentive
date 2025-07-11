935347bda8d31b5c668ae48ea497b5fc3c5aa6d0
Niklas Kloth <niklas.kloth@gmail.com>
2025-07-11 11:09:06 +0200

    corrected footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
                 <div className="lg:flex">
                     <div className="w-full lg:w-1/4 ">
                         <h3 className="text-2xl font-semibold text-white">About Us</h3>
-                        <p className="mt-2 text-gray-400">AGENTIVE - A platform for multilingual education.</p>
+                        <p className="mt-2 text-gray-400">AGENTIVE - A platform for multilingual education</p>
                     </div>
                     <div className="w-full lg:w-1/4">
                         <h3 className="text-2xl font-semibold text-white">Contact Us</h3>
