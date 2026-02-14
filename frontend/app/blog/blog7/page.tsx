c12caa7e7444af3f8ef8fa6948d0a923d43b457e
Anton Lechtreck <79366702+aflechtreck@users.noreply.github.com>
2026-02-14 14:14:13 +0100

    Meet Bobba and Meeth the creators blogs

---
+++ frontend/app/blog/blog7/page.tsx
@@ -0,0 +1,165 @@
+import Image from "next/image";
+
+export default function About() {
+  const translations = [
+    { language: "Albanian", translator: "Valmire Ymeri" },
+    { language: "French", translator: "Carole Dording, Jeanne\n" +
+            "Letsch, Angélique Quintus\n" +
+            "(UNILU)" },
+    { language: "German-Long", translator: "Christina vom Brocke\n" +
+            "(PHGR)" },
+    { language: "German", translator: "Christina vom Brocke\n" +
+            "(PHGR), Marjan Asgari\n" +
+            "(UNIBZ), Nancy Morys\n" +
+            "(UNILU)" },
+      { language: "Greek", translator: "Korina Defteraiou, Stefania\n" +
+              "Oikonomou (W2L)" },
+      { language: "Hungarian", translator: "Beti Karakatič, \n" +
+              "Annamária Gróg" },
+      { language: "Italian", translator: "Eleonora Conci,\n" +
+              "Francesca Cangemi\n" +
+              "(PHGR)" },
+      { language: "Ladin", translator: "Sofia Stuflesser" },
+      { language: "Russian", translator: "Alisa Saveleva " },
+      { language: "Luxembourgish", translator: "Carole Dording, Jeanne\n" +
+              "Letsch, Angélique Quintus\n" +
+              "(UNILU)" },
+      { language: "Polish", translator: "Magdalena Gösken" },
+      { language: "Portuguese", translator: "Sofia Alvares (UNILU)" },
+      { language: "Rumantsch Grischun", translator: "Irina Lutz (PHGR)" },
+      { language: "Slovenian", translator: "Anja Pirih (UP),\n" +
+              "Silva Bratož (UP),\n" +
+              "Mojca Žefran (UP)" },
+      { language: "Sursilvan", translator: "Irina Lutz (PHGR)" },
+      { language: "Turkish", translator: "Banu Can Schürmann" },
+      { language: "Ukrainian", translator: "Kateryna Boichenko (W2L)" },
+  ];
+
+
+  const recorders = [
+    { language: "Albanian", recorder: "Elza Hoti, Nicolas, \n" +
+          "Donnerup (UNILU) " },
+       { language: "English", recorder: "Claudine Tscharner\n" +
+          "(PHGR)"},
+    { language: "French", recorder: "Laetitia Collin (UNILU) " },
+
+    { language: "German-Long", recorder: "Christina vom Brocke\n" +
+            "(PHGR)" },
+    { language: "German", recorder: "Nancy Morys (UNILU) " },
+      { language: "Greek", recorder: "Stefania Oikonomou\n" +
+            "(W2L) " },
+      { language: "Hungarian", recorder: "Beti Karakatič"},
+      { language: "Italian", recorder: "Francesca Cangemi (PHGR)" },
+      { language: "Ladin", recorder: "Sofia Stuflesser" },
+
+      { language: "Luxembourgish", recorder: "Jeanne Letsch (UNILU)" },
+      { language: "Polish", recorder: "Magdalena Gösken" },
+      { language: "Portuguese", recorder: "Mélanie Goncalves\n" +
+            "Marques (UNILU)" },
+      { language: "Rumantsch Grischun", recorder: "Irina Lutz (PHGR)" },
+      { language: "Russian", recorder: "Alisa Saveleva " },
+      { language: "Slovenian", recorder: "Anja Pirih (UP)" },
+      { language: "Sursilvan", recorder: "Irina Lutz (PHGR)" },
+      { language: "Turkish", recorder: "Banu Can Schürmann" },
+      { language: "Ukrainian", recorder: "Kateryna Boichenko (W2L)" },
+  ];
+
+  return (
+    <div className="container mx-auto py-8 px-8">
+      <h1 className="text-3xl font-bold mb-6">Story 4 - Sleeping at a friend’s</h1>
+
+      <h2 className="text-xl font-semibold mb-4">
+        Meet the authors and contributors that wrote the fourth story about Bobba!
+      </h2>
+
+      <div className="flex flex-col md:flex-row gap-8 items-center mb-12">
+        {/* Image section */}
+        <div className="md:w-1/2">
+          <Image
+            src="/learning-material/story-4/story-en/Story 4_2 title_E.jpg"
+            alt="Bobba the alien"
+            width={500}
+            height={500}
+            className="rounded-lg shadow-lg"
+          />
+        </div>
+
+        {/* Text section */}
+<div className="md:w-1/2">
+  <p className="text-gray-600 mb-6 dark:text-white leading-relaxed">
+    In this fourth installment, Bobba experiences his very first sleepover!
+    Joining his friend Olivia, Bobba learns all about nighttime
+    routines and the joy of friendship on Earth. This heartwarming chapter is
+    brought to life through our global community, and we want to give a
+     big thanks to all the translators and recorders for
+    participating in making this story possible!
+    <br/><br/>
+    Follow Bobba and Olivia's nighttime adventure in "Sleeping at a friend’s,"
+    available on the AGENTIVE Platform in multiple languages thanks to the
+    dedicated contributors listed below.
+  </p>
+
+  {/* Project Credits */}
+  <div className="text-gray-800 dark:text-gray-100 space-y-1 border-t pt-4 border-gray-200 dark:border-gray-700">
+    <p><span className="font-bold">Author(s):</span>Angélique Quintus, Claudine Kirsch,
+      Jeanne Letsch (UNILU)</p>
+    <p><span className="font-bold">Peer-reviewed:</span> UNILU, UNIBZ, UNIPRI</p>
+    <p><span className="font-bold">Pictures:</span> Katerina Veroutos and Angélique Quintus</p>
+  </div>
+</div>
+      </div>
+
+      {/* Translations Table */}
+      <div className="mt-10 overflow-x-auto">
+        <h3 className="text-2xl font-bold mb-4">Translation Contributors</h3>
+        <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
+          <thead>
+          <tr className="bg-gray-100 dark:bg-gray-700 text-left">
+            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Language</th>
+            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Translator</th>
+          </tr>
+          </thead>
+          <tbody className="text-gray-700 dark:text-gray-200">
+          {translations.map((item, index) => (
+              <tr key={index} className="border-b hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                <td className="py-4 px-6">{item.language}</td>
+                <td className="py-4 px-6">{item.translator}</td>
+              </tr>
+          ))}
+          </tbody>
+
+
+        </table>
+
+
+
+      </div>
+
+      {/* Translations Table */}
+      <div className="mt-10 overflow-x-auto">
+        <h3 className="text-2xl font-bold mb-4">Audio Recording Contributors</h3>
+        <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
+          <thead>
+          <tr className="bg-gray-100 dark:bg-gray-700 text-left">
+            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Language</th>
+            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Translator</th>
+          </tr>
+          </thead>
+          <tbody className="text-gray-700 dark:text-gray-200">
+          {recorders.map((item, index) => (
+              <tr key={index} className="border-b hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                <td className="py-4 px-6">{item.language}</td>
+                <td className="py-4 px-6">{item.recorder}</td>
+              </tr>
+          ))}
+          </tbody>
+
+
+        </table>
+
+
+
+      </div>
+    </div>
+  );
+}
--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -5,7 +5,15 @@ export default function Blog() {
   const blogPosts = [
     { title: 'Kickoff - Enable Multilinguism', excerpt: 'We just started our journey. Learn about what we want to achive!', image: '/placeholder.svg', link: '/blog/blog1' },
     { title: 'Website going live', excerpt: 'We just launched the website you can see here. Learn more here', image: '/placeholder.svg', link: '/blog/blog2' },
-      { title: 'Meet Bobba!', excerpt: 'Learn who Bobba is!', image: '/learning-material/story-1/story-en/Story1_title_E.webp', link: '/blog/blog3' }
+      { title: 'Meet Bobba!', excerpt: 'Learn who Bobba is!', image: '/learning-material/story-1/story-en/Story1_title_E.webp', link: '/blog/blog3' },
+      { title: 'Story 1 - Meet the Creators!', excerpt: 'Bobba\'s first trip to earth', image: '/learning-material/story-1/story-en/Story1_title_E.webp', link: '/blog/blog4' },
+      { title: 'Story 2 - Meet the Creators!', excerpt: 'At the kindergarten', image: '/learning-material/story-2/story-en/Story 2_title_E.jpg', link: '/blog/blog5' },
+      { title: 'Story 3 - Meet the Creators!', excerpt: 'Bobba at the beach', image: '/learning-material/story-3/story-title/Story 3_COVER_E.JPG', link: '/blog/blog6' },
+      { title: 'Story 4 - Meet the Creators!', excerpt: 'Sleeping at a friend\'s', image: '/learning-material/story-4/story-en/Story 4_2 title_E.jpg', link: '/blog/blog7' },
+      { title: 'Story 5 - Meet the Creators!', excerpt: 'Bobba in the library', image: '/learning-material/story-5/story-en/Story 5_title_E.jpg', link: '/blog/blog8' },
+      { title: 'Story 6 - Meet the Creators!', excerpt: 'The Monster castle', image: '/learning-material/story-6/covers/Story 6_title_E.jpg', link: '/blog/blog9' },
+      { title: 'Story 7 - Meet the Creators!', excerpt: 'Celebrating Olivia\'s birthday', image: '/learning-material/story-7/covers/Story 7_title_E.jpg', link: '/blog/blog10' },
+      { title: 'Story 8 - Meet the Creators!', excerpt: 'Going home', image: '/learning-material/story-8/covers/Story 8_title_E.jpg', link: '/blog/blog11' }
   ]
 
   return (
