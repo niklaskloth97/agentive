cb3235bfbb31e09949aa9310f44f5968e4d878cb
Niklas Kloth <niklas.kloth@gmail.com>
2026-07-08 10:59:56 +0200

    changed blog images

--- frontend/app/blog/blog12/page.tsx
+++ frontend/app/blog/blog12/page.tsx
@@ -8,13 +8,12 @@ export default function MilestoneBlog() {
     <div className="container mx-auto py-12 px-8 max-w-5xl">
       {/* Header Section */}
       <header className="text-center mb-12">
-
         <h1 className="text-4xl md:text-5xl font-extrabold mt-2 mb-6">
           Bobba Speaks Your Language! 🌍
         </h1>
         <p className="text-xl text-gray-700 dark:text-gray-300 max-w-2xl mx-auto">
-          We&apos;ve just hit a major orbit: Bobba&apos;s stories are now available in 15 languages!
-
+          We&apos;ve just hit a major orbit: Bobba&apos;s stories are now
+          available in 15 languages!
         </p>
       </header>
 
@@ -22,7 +21,7 @@ export default function MilestoneBlog() {
         {/* Image section */}
         <div className="md:w-1/2 group">
           <Image
-            src="/learning-material/story-1/story-en/Story1_title_E.webp"
+            src="/learning-material/story-1/story-en/Story1_title_E.jpeg"
             alt="Bobba the alien celebrating"
             width={600}
             height={600}
@@ -32,16 +31,19 @@ export default function MilestoneBlog() {
 
         {/* Text section */}
         <div className="md:w-1/2">
-          <h2 className="text-2xl font-bold mb-4">Breaking the Language Barrier</h2>
+          <h2 className="text-2xl font-bold mb-4">
+            Breaking the Language Barrier
+          </h2>
           <p className="text-gray-600 dark:text-gray-300 mb-6 leading-relaxed">
-            Bobba&apos;s journey from a planet far, far away just got a lot more accessible.
-            To ensure every child can accompany him on his visit to Earth, we have expanded
-            the AGENTIVE Platform to support 15 different languages from across the European continent.
-         </p>
+            Bobba&apos;s journey from a planet far, far away just got a lot more
+            accessible. To ensure every child can accompany him on his visit to
+            Earth, we have expanded the AGENTIVE Platform to support 15
+            different languages from across the European continent.
+          </p>
           <p>
-              Language is the key to the stars. Now, Bobba can say &apos;Hello&apos; to almost everyone in Europe!
-            </p>
-
+            Language is the key to the stars. Now, Bobba can say
+            &apos;Hello&apos; to almost everyone in Europe!
+          </p>
         </div>
       </div>
 
@@ -62,12 +64,13 @@ export default function MilestoneBlog() {
       </div>
 
       <div className="mt-16 text-center">
-        <p className="text-lg mb-6">This is just the beginning of our multilingual mission.</p>
+        <p className="text-lg mb-6">
+          This is just the beginning of our multilingual mission.
+        </p>
 
         {/* Add your destination path in the href attribute below */}
         <Link href="/dashboard">
-          <button
-              className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full transition-colors">
+          <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full transition-colors">
             Explore the Stories
           </button>
         </Link>
--- frontend/app/blog/blog6/page.tsx
+++ frontend/app/blog/blog6/page.tsx
@@ -3,65 +3,87 @@ import Image from "next/image";
 export default function About() {
   const translations = [
     { language: "Albanian", translator: "Valmire Ymeri" },
-    { language: "French", translator: "Carole Dording, Jeanne\n" +
-            "Letsch, Angélique Quintus\n" +
-            "(UNILU)" },
-    { language: "German-Long", translator: "Christina vom Brocke\n" +
-            "(PHGR)" },
-    { language: "German", translator: "Christina vom Brocke\n" +
-            "(PHGR), Marjan Asgari\n" +
-            "(UNIBZ), Nancy Morys\n" +
-            "(UNILU)" },
-      { language: "Greek", translator: "Korina Defteraiou, Stefania\n" +
-              "Oikonomou (W2L)" },
-      { language: "Hungarian", translator: "Beti Karakatič, \n" +
-              "Annamária Gróg" },
-      { language: "Italian", translator: "Eleonora Conci,\n" +
-              "Francesca Cangemi\n" +
-              "(PHGR)" },
-      { language: "Ladin", translator: "Sofia Stuflesser" },
-      { language: "Russian", translator: "Alisa Saveleva " },
-      { language: "Luxembourgish", translator: "Carole Dording, Jeanne\n" +
-              "Letsch, Angélique Quintus\n" +
-              "(UNILU)" },
-      { language: "Polish", translator: "Magdalena Gösken" },
-      { language: "Portuguese", translator: "Sofia Alvares (UNILU)" },
-      { language: "Rumantsch Grischun", translator: "Irina Lutz (PHGR)" },
-      { language: "Slovenian", translator: "Anja Pirih (UP),\n" +
-              "Silva Bratož (UP),\n" +
-              "Mojca Žefran (UP)" },
-      { language: "Sursilvan", translator: "Irina Lutz (PHGR)" },
-      { language: "Turkish", translator: "Banu Can Schürmann" },
-      { language: "Ukrainian", translator: "Kateryna Boichenko (W2L)" },
+    {
+      language: "French",
+      translator:
+        "Carole Dording, Jeanne\n" + "Letsch, Angélique Quintus\n" + "(UNILU)",
+    },
+    {
+      language: "German-Long",
+      translator: "Christina vom Brocke\n" + "(PHGR)",
+    },
+    {
+      language: "German",
+      translator:
+        "Christina vom Brocke\n" +
+        "(PHGR), Marjan Asgari\n" +
+        "(UNIBZ), Nancy Morys\n" +
+        "(UNILU)",
+    },
+    {
+      language: "Greek",
+      translator: "Korina Defteraiou, Stefania\n" + "Oikonomou (W2L)",
+    },
+    {
+      language: "Hungarian",
+      translator: "Beti Karakatič, \n" + "Annamária Gróg",
+    },
+    {
+      language: "Italian",
+      translator: "Eleonora Conci,\n" + "Francesca Cangemi\n" + "(PHGR)",
+    },
+    { language: "Ladin", translator: "Sofia Stuflesser" },
+    { language: "Russian", translator: "Alisa Saveleva " },
+    {
+      language: "Luxembourgish",
+      translator:
+        "Carole Dording, Jeanne\n" + "Letsch, Angélique Quintus\n" + "(UNILU)",
+    },
+    { language: "Polish", translator: "Magdalena Gösken" },
+    { language: "Portuguese", translator: "Sofia Alvares (UNILU)" },
+    { language: "Rumantsch Grischun", translator: "Irina Lutz (PHGR)" },
+    {
+      language: "Slovenian",
+      translator:
+        "Anja Pirih (UP),\n" + "Silva Bratož (UP),\n" + "Mojca Žefran (UP)",
+    },
+    { language: "Sursilvan", translator: "Irina Lutz (PHGR)" },
+    { language: "Turkish", translator: "Banu Can Schürmann" },
+    { language: "Ukrainian", translator: "Kateryna Boichenko (W2L)" },
+    { language: "Chinese", translator: "Sisi Laoshi" },
   ];
 
-
   const recorders = [
-    { language: "Albanian", recorder: "Elza Hoti, Nicolas, \n" +
-          "Donnerup (UNILU) " },
-       { language: "English", recorder: "Claudine Tscharner\n" +
-          "(PHGR)"},
+    {
+      language: "Albanian",
+      recorder: "Elza Hoti, Nicolas, \n" + "Donnerup (UNILU) ",
+    },
+    { language: "English", recorder: "Claudine Tscharner\n" + "(PHGR)" },
     { language: "French", recorder: "Laetitia Collin (UNILU) " },
 
-    { language: "German-Long", recorder: "Christina vom Brocke\n" +
-            "(PHGR)" },
+    { language: "German-Long", recorder: "Christina vom Brocke\n" + "(PHGR)" },
     { language: "German", recorder: "Nancy Morys (UNILU) " },
-      { language: "Greek", recorder: "Stefania Oikonomou\n" +
-            "(W2L) " },
-      { language: "Hungarian", recorder: "Beti Karakatič"},
-      { language: "Italian", recorder: "Francesca Cangemi (PHGR)" },
-      { language: "Ladin", recorder: "Sofia Stuflesser" },
+    { language: "Greek", recorder: "Stefania Oikonomou\n" + "(W2L) " },
+    { language: "Hungarian", recorder: "Beti Karakatič" },
+    { language: "Italian", recorder: "Francesca Cangemi (PHGR)" },
+    { language: "Ladin", recorder: "Sofia Stuflesser" },
 
-      { language: "Luxembourgish", recorder: "Jeanne Letsch (UNILU)" },
-      { language: "Polish", recorder: "Magdalena Gösken" },
-      { language: "Portuguese", recorder: "Mélanie Goncalves\n" +
-            "Marques (UNILU)" },
-      { language: "Rumantsch Grischun", recorder: "Irina Lutz (PHGR)" },
-      { language: "Russian", recorder: "Alisa Saveleva " },
-      { language: "Slovenian", recorder: "Anja Pirih (UP)" },
-      { language: "Sursilvan", recorder: "Irina Lutz (PHGR)" },
-      { language: "Turkish", recorder: "Banu Can Schürmann" },
-      { language: "Ukrainian", recorder: "Kateryna Boichenko (W2L)" },
+    { language: "Luxembourgish", recorder: "Jeanne Letsch (UNILU)" },
+    { language: "Polish", recorder: "Magdalena Gösken" },
+    {
+      language: "Portuguese",
+      recorder: "Mélanie Goncalves\n" + "Marques (UNILU)",
+    },
+    { language: "Rumantsch Grischun", recorder: "Irina Lutz (PHGR)" },
+    { language: "Russian", recorder: "Alisa Saveleva " },
+    { language: "Slovenian", recorder: "Anja Pirih (UP)" },
+    { language: "Sursilvan", recorder: "Irina Lutz (PHGR)" },
+    { language: "Turkish", recorder: "Banu Can Schürmann" },
+    { language: "Ukrainian", recorder: "Kateryna Boichenko (W2L)" },
+    { language: "Croatian", recorder: "Kristina Riman" },
+    { language: "Chinese", recorder: "Sisi Laoshi\n" + "Fiona Cees\n" + "Isla Van Oosterom Devlin\n" + "Luis Ziegler" },
+
+
   ];
 
   return (
@@ -69,7 +91,8 @@ export default function About() {
       <h1 className="text-3xl font-bold mb-6">Story 3 - Bobba at the beach</h1>
 
       <h2 className="text-xl font-semibold mb-4">
-        Meet the authors and contributors that wrote the third story about Bobba!
+        Meet the authors and contributors that wrote the third story about
+        Bobba!
       </h2>
 
       <div className="flex flex-col md:flex-row gap-8 items-center mb-12">
@@ -86,26 +109,36 @@ export default function About() {
 
         {/* Text section */}
         {/* Text section */}
-<div className="md:w-1/2">
-  <p className="text-gray-600 mb-6 dark:text-white leading-relaxed">
-    In this third installment, Bobba experiences the wonders of the seaside! From
-    sandy shores to rolling waves, this sunny chapter of his journey is brought to life
-    through our collaborative network. We want to give a big thanks to all
-    the translators and recorders for their incredible work in making this
-    beach adventure accessible!
-    <br/><br/>
-    Join Bobba as he explores the coastal world in &quot;Bobba at the Beach&quot; available
-    in various languages on the AGENTIVE Platform thanks to the dedicated
-    contributors listed below.
-  </p>
+        <div className="md:w-1/2">
+          <p className="text-gray-600 mb-6 dark:text-white leading-relaxed">
+            In this third installment, Bobba experiences the wonders of the
+            seaside! From sandy shores to rolling waves, this sunny chapter of
+            his journey is brought to life through our collaborative network. We
+            want to give a big thanks to all the translators and recorders for
+            their incredible work in making this beach adventure accessible!
+            <br />
+            <br />
+            Join Bobba as he explores the coastal world in &quot;Bobba at the
+            Beach&quot; available in various languages on the AGENTIVE Platform
+            thanks to the dedicated contributors listed below.
+          </p>
 
-  {/* Project Credits */}
-  <div className="text-gray-800 dark:text-gray-100 space-y-1 border-t pt-4 border-gray-200 dark:border-gray-700">
-    <p><span className="font-bold">Author(s):</span> Anja Pirih (UP) and Silva Bratož (UP)</p>
-    <p><span className="font-bold">Peer-reviewed:</span> UNILU, UNIBZ, UNIPRI</p>
-    <p><span className="font-bold">Pictures:</span> Katerina Veroutos, Anja Pirih (UP) and Silva Bratož (UP)</p>
-  </div>
-</div>
+          {/* Project Credits */}
+          <div className="text-gray-800 dark:text-gray-100 space-y-1 border-t pt-4 border-gray-200 dark:border-gray-700">
+            <p>
+              <span className="font-bold">Author(s):</span> Anja Pirih (UP) and
+              Silva Bratož (UP)
+            </p>
+            <p>
+              <span className="font-bold">Peer-reviewed:</span> UNILU, UNIBZ,
+              UNIPRI
+            </p>
+            <p>
+              <span className="font-bold">Pictures:</span> Katerina Veroutos,
+              Anja Pirih (UP) and Silva Bratož (UP)
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* Translations Table */}
@@ -113,51 +146,57 @@ export default function About() {
         <h3 className="text-2xl font-bold mb-4">Translation Contributors</h3>
         <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
           <thead>
-          <tr className="bg-gray-100 dark:bg-gray-700 text-left">
-            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Language</th>
-            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Translator</th>
-          </tr>
+            <tr className="bg-gray-100 dark:bg-gray-700 text-left">
+              <th className="py-3 px-6 font-semibold uppercase text-sm border-b">
+                Language
+              </th>
+              <th className="py-3 px-6 font-semibold uppercase text-sm border-b">
+                Translator
+              </th>
+            </tr>
           </thead>
           <tbody className="text-gray-700 dark:text-gray-200">
-          {translations.map((item, index) => (
-              <tr key={index} className="border-b hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+            {translations.map((item, index) => (
+              <tr
+                key={index}
+                className="border-b hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors"
+              >
                 <td className="py-4 px-6">{item.language}</td>
                 <td className="py-4 px-6">{item.translator}</td>
               </tr>
-          ))}
+            ))}
           </tbody>
-
-
         </table>
-
-
-
       </div>
 
       {/* Translations Table */}
       <div className="mt-10 overflow-x-auto">
-        <h3 className="text-2xl font-bold mb-4">Audio Recording Contributors</h3>
+        <h3 className="text-2xl font-bold mb-4">
+          Audio Recording Contributors
+        </h3>
         <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
           <thead>
-          <tr className="bg-gray-100 dark:bg-gray-700 text-left">
-            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Language</th>
-            <th className="py-3 px-6 font-semibold uppercase text-sm border-b">Translator</th>
-          </tr>
+            <tr className="bg-gray-100 dark:bg-gray-700 text-left">
+              <th className="py-3 px-6 font-semibold uppercase text-sm border-b">
+                Language
+              </th>
+              <th className="py-3 px-6 font-semibold uppercase text-sm border-b">
+                Translator
+              </th>
+            </tr>
           </thead>
           <tbody className="text-gray-700 dark:text-gray-200">
-          {recorders.map((item, index) => (
-              <tr key={index} className="border-b hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+            {recorders.map((item, index) => (
+              <tr
+                key={index}
+                className="border-b hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors"
+              >
                 <td className="py-4 px-6">{item.language}</td>
                 <td className="py-4 px-6">{item.recorder}</td>
               </tr>
-          ))}
+            ))}
           </tbody>
-
-
         </table>
-
-
-
       </div>
     </div>
   );
